d21f6a672619622e4c86894147ec0e88565a9fb1
alpar <szabadosalpar@gmail.com>
2016-09-15 11:46:43 +0300

    updated the Config, now folds input and output file locations as well as save, overwrite and date messages

--- src/main/groovy/szabados/alpar/Config.groovy
+++ src/main/groovy/szabados/alpar/Config.groovy
@@ -1,5 +1,13 @@
 package szabados.alpar
 
+import static szabados.alpar.CurrentDate.TODAY
+
 class Config {
     static final QUESTIONS = new File('questions.txt').text
+    static final INSIGNIA = new File('insignia.txt').text
+    static final LOG = new File("${TODAY}.txt")
+
+    static final SAVE_MESSAGE = "Do you want to save today's progress? (y/n)"
+    static final OVERWRITE_MESSAGE = "You already saved your progress today, do you want to overwrite?"
+    static final STARDATE = "Stardate: ${TODAY}\n\nCaptain's Log:\n\n"
 }
