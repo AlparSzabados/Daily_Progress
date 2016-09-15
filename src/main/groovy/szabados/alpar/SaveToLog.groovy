a7feb611caddf470a2cc2e9741665a7e57c7336f
alpar <szabadosalpar@gmail.com>
2016-09-15 14:04:57 +0300

    Added insignia to the LOG.

--- src/main/groovy/szabados/alpar/SaveToLog.groovy
+++ src/main/groovy/szabados/alpar/SaveToLog.groovy
@@ -1,13 +1,15 @@
 package szabados.alpar
 
+import static szabados.alpar.Config.INSIGNIA
 import static szabados.alpar.Config.LOG
+import static szabados.alpar.CurrentDate.TODAY
 import static szabados.alpar.UserInput.userInput
 
 class SaveToLog {
     static saveLog(answers, String messageYes, String messageNo) {
         boolean userYes = userInput().toLowerCase() == 'y'
         if (userYes) {
-            LOG.write(answers.toString())
+            LOG.write("${INSIGNIA}\n${TODAY}\n\nToday's progress:\n${answers.toString()}")
             println messageYes
         } else
             println messageNo
